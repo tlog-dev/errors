3df722064d8f9f539e83aaece6e3154819dcef96
nikandfor <nikandfor@gmail.com>
2021-12-09 15:30:11 +0300

    update loc; fix tests

--- fmt_test.go
+++ fmt_test.go
@@ -13,7 +13,7 @@ func TestErrorFormatLocation(t *testing.T) {
 
 	assert.Equal(t, "inner", fmt.Sprintf("%v", err))
 	assert.Equal(t, "inner (fmt_test.go:12)", fmt.Sprintf("%+v", err))
-	assert.Regexp(t, "inner at (github.com/nikandfor/errors/)*fmt_test.go:12", fmt.Sprintf("% +v", err))
+	assert.Regexp(t, "inner at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:12", fmt.Sprintf("% +v", err))
 
 	// more
 	err = Wrap(
@@ -22,8 +22,8 @@ func TestErrorFormatLocation(t *testing.T) {
 
 	assert.Equal(t, "global: middle: file does not exist", fmt.Sprintf("%v", err))
 	assert.Equal(t, "global (fmt_test.go:19): middle (fmt_test.go:20): file does not exist", fmt.Sprintf("%+v", err))
-	assert.Regexp(t, `global at (github.com/nikandfor/errors/)*fmt_test.go:19
-middle at (github.com/nikandfor/errors/)*fmt_test.go:20
+	assert.Regexp(t, `global at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:19
+middle at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:20
 file does not exist`, fmt.Sprintf("% +v", err))
 
 	// one more
@@ -35,9 +35,9 @@ file does not exist`, fmt.Sprintf("% +v", err))
 
 	assert.Equal(t, "global: middle: inner", fmt.Sprintf("%v", err))
 	assert.Equal(t, "global (fmt_test.go:30): middle (fmt_test.go:31): inner (fmt_test.go:32)", fmt.Sprintf("%+v", err))
-	assert.Regexp(t, `global at (github.com/nikandfor/errors/)*fmt_test.go:30
-middle at (github.com/nikandfor/errors/)*fmt_test.go:31
-inner at (github.com/nikandfor/errors/)*fmt_test.go:32`, fmt.Sprintf("% +v", err))
+	assert.Regexp(t, `global at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:30
+middle at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:31
+inner at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:32`, fmt.Sprintf("% +v", err))
 
 	// with no messages
 	err = Wrap(
@@ -48,9 +48,9 @@ inner at (github.com/nikandfor/errors/)*fmt_test.go:32`, fmt.Sprintf("% +v", err
 
 	assert.Equal(t, nomessage, fmt.Sprintf("%v", err))
 	assert.Equal(t, fmt.Sprintf("%v (fmt_test.go:43): %[1]v (fmt_test.go:44): %[1]v (fmt_test.go:45)", nomessage), fmt.Sprintf("%+v", err))
-	assert.Regexp(t, `\(no message\) at (github.com/nikandfor/errors/)*fmt_test.go:43
-\(no message\) at (github.com/nikandfor/errors/)*fmt_test.go:44
-\(no message\) at (github.com/nikandfor/errors/)*fmt_test.go:45`, fmt.Sprintf("% +v", err))
+	assert.Regexp(t, `\(no message\) at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:43
+\(no message\) at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:44
+\(no message\) at (((github.com/)?nikandfor/)?errors/)?fmt_test.go:45`, fmt.Sprintf("% +v", err))
 }
 
 func TestErrorFormat(t *testing.T) {
