ff607dd60c7401c27e3cf974e76cbd9c77a4466a
nikandfor <nikandfor@gmail.com>
2022-03-01 01:16:20 +0300

    printArg fallback

--- unsafe_fmt.go
+++ unsafe_fmt.go
@@ -2,25 +2,81 @@ package errors
 
 import (
 	"fmt"
-	"os"
-	"reflect"
+	"io"
+	"strconv"
 	"unsafe"
 )
 
 func subPrintArg(s fmt.State, arg interface{}, verb rune) {
-	if tp := reflect.TypeOf(s); tp.String() != "*fmt.pp" {
-		fmt.Fprintf(os.Stderr, "aborting subprint (state type: %v)\n", tp.String())
+	i := *(*iface)(unsafe.Pointer(&s))
+	if i.typ != ppType {
+		var buf [64]byte
+
+		i := 0
+
+		buf[i] = '%'
+		i++
+
+		for _, f := range "-+# 0" {
+			if s.Flag(int(f)) {
+				buf[i] = byte(f)
+				i++
+			}
+		}
+
+		if w, ok := s.Width(); ok {
+			q := strconv.AppendInt(buf[:i], int64(w), 10)
+			i = len(q)
+		}
+
+		if p, ok := s.Precision(); ok {
+			buf[i] = '.'
+			i++
+
+			q := strconv.AppendInt(buf[:i], int64(p), 10)
+			i = len(q)
+		}
+
+		buf[i] = byte(verb)
+		i++
+
+		fmt.Fprintf(s, bytesToString(buf[:i]), arg)
+
 		return
 	}
-
-	type iface struct {
-		typ, word unsafe.Pointer
-	}
-
-	i := *(*iface)(unsafe.Pointer(&s))
 
 	printArg(i.word, arg, verb)
 }
 
+type (
+	iface struct {
+		typ, word unsafe.Pointer
+	}
+
+	formatter struct{}
+)
+
+var ppType unsafe.Pointer
+
+func init() {
+	fmt.Fprintf(io.Discard, "%v", formatter{})
+}
+
 //go:linkname printArg fmt.(*pp).printArg
 func printArg(p unsafe.Pointer, arg interface{}, verb rune)
+
+//go:linkname newPrinter fmt.newPrinter
+func newPrinter() unsafe.Pointer
+
+//go:linkname ppFree fmt.(*pp).free
+func ppFree(unsafe.Pointer)
+
+func (formatter) Format(s fmt.State, c rune) {
+	i := *(*iface)(unsafe.Pointer(&s))
+
+	ppType = i.typ
+}
+
+func bytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
---
+++ unsafe_fmt_test.go
@@ -0,0 +1,128 @@
+package errors
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type (
+	testformatter struct {
+		flags [64]bool
+
+		wid int
+		prc int
+
+		widok bool
+		prcok bool
+
+		verb rune
+
+		buf []byte
+	}
+)
+
+func TestSubPrintArg(t *testing.T) {
+	var b bytes.Buffer
+
+	var f testformatter
+
+	fmt.Fprintf(&b, "%+012.6q", &f)
+
+	assert.Equal(t, testformatter{
+		flags: flags("+0"),
+		wid:   12,
+		widok: true,
+		prc:   6,
+		prcok: true,
+		verb:  'q',
+	}, f)
+
+	//
+
+	f = testformatter{}
+	f2 := testformatter{
+		flags: flags("-# "),
+		prc:   3,
+		prcok: true,
+	}
+
+	subPrintArg(&f2, &f, 'v')
+
+	assert.Equal(t, testformatter{
+		flags: flags(" -#"),
+		prc:   3,
+		prcok: true,
+		verb:  'v',
+	}, f)
+}
+
+func BenchmarkPringArg(b *testing.B) {
+	b.ReportAllocs()
+
+	f := testformatter{}
+
+	for i := 0; i < b.N; i++ {
+		pp := newPrinter()
+		printArg(pp, &f, 'v')
+		ppFree(pp)
+	}
+}
+
+func BenchmarkPringArgFallback(b *testing.B) {
+	b.ReportAllocs()
+
+	f := testformatter{}
+	f2 := testformatter{
+		flags: flags("-# "),
+		prc:   3,
+		prcok: true,
+	}
+
+	for i := 0; i < b.N; i++ {
+		subPrintArg(&f2, &f, 'v')
+	}
+}
+
+func (f *testformatter) Format(s fmt.State, verb rune) {
+	f.flags = [64]bool{}
+
+	for _, q := range "-+# 0" {
+		if s.Flag(int(q)) {
+			f.flags[q] = true
+		}
+	}
+
+	f.wid, f.widok = s.Width()
+	f.prc, f.prcok = s.Precision()
+
+	f.verb = verb
+}
+
+func (f *testformatter) Flag(c int) bool {
+	return f.flags[c]
+}
+
+func (f *testformatter) Width() (int, bool) {
+	return f.wid, f.widok
+}
+
+func (f *testformatter) Precision() (int, bool) {
+	return f.prc, f.prcok
+}
+
+func (f *testformatter) Write(p []byte) (int, error) {
+	f.buf = append(f.buf, p...)
+
+	return len(p), nil
+}
+
+func flags(f string) (r [64]bool) {
+	for _, q := range f {
+		r[q] = true
+	}
+
+	return
+}
