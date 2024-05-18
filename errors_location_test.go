f1c00cc338950cd20b261d725f861caed041e27c
nikandfor <nikandfor@gmail.com>
2024-05-18 20:40:08 +0200

    linter fixes

--- errors_location_test.go
+++ errors_location_test.go
@@ -8,13 +8,13 @@ import (
 )
 
 func TestDepthCaller(t *testing.T) {
-	assert.Equal(t, "errors_location_test.go:11", NewDepth(0, "msg").(Callerer).Caller().String())              //nolint:errorlint
-	assert.Equal(t, "errors_location_test.go:12", NewCaller(loc.Caller(0), "msg").(Callerer).Caller().String()) //nolint:errorlint
+	assert.Equal(t, "errors_location_test.go:11", NewDepth(0, "msg").(Callerer).Caller().String())
+	assert.Equal(t, "errors_location_test.go:12", NewCaller(loc.Caller(0), "msg").(Callerer).Caller().String())
 
 	err := New("inner")
 
-	assert.Equal(t, "errors_location_test.go:16", WrapDepth(err, 0, "msg").(Callerer).Caller().String())              //nolint:errorlint
-	assert.Equal(t, "errors_location_test.go:17", WrapCaller(err, loc.Caller(0), "msg").(Callerer).Caller().String()) //nolint:errorlint
+	assert.Equal(t, "errors_location_test.go:16", WrapDepth(err, 0, "msg").(Callerer).Caller().String())
+	assert.Equal(t, "errors_location_test.go:17", WrapCaller(err, loc.Caller(0), "msg").(Callerer).Caller().String())
 }
 
 func TestStackCallers(t *testing.T) {
@@ -22,15 +22,15 @@ func TestStackCallers(t *testing.T) {
 }
 
 func testStackCallers(t *testing.T) {
-	assert.Equal(t, "errors_location_test.go:25", NewStack(0, 2, "msg").(Callerer).Caller().String())                //nolint:errorlint
-	assert.Equal(t, "errors_location_test.go:26", NewCallers(loc.Callers(0, 2), "msg").(Callerer).Caller().String()) //nolint:errorlint
+	assert.Equal(t, "errors_location_test.go:25", NewStack(0, 2, "msg").(Callerer).Caller().String())
+	assert.Equal(t, "errors_location_test.go:26", NewCallers(loc.Callers(0, 2), "msg").(Callerer).Caller().String())
 
 	err := New("inner")
 
-	assert.Equal(t, "errors_location_test.go:30", WrapStack(err, 0, 2, "msg").(Callerer).Caller().String())                //nolint:errorlint
-	assert.Equal(t, "errors_location_test.go:31", WrapCallers(err, loc.Callers(0, 2), "msg").(Callerer).Caller().String()) //nolint:errorlint
+	assert.Equal(t, "errors_location_test.go:30", WrapStack(err, 0, 2, "msg").(Callerer).Caller().String())
+	assert.Equal(t, "errors_location_test.go:31", WrapCallers(err, loc.Callers(0, 2), "msg").(Callerer).Caller().String())
 
-	assert.Equal(t, PC(0), NewCallers(PCs{}, "msg").(Callerer).Caller())                                //nolint:errorlint
-	assert.Equal(t, "errors_location_test.go:34", NewStack(0, 1, "msg").(Callerer).Caller().String())   //nolint:errorlint
-	assert.Equal(t, "errors_location_test.go:35", NewStack(0, 1, "msg").(Callerser).Callers().String()) //nolint:errorlint
+	assert.Equal(t, PC(0), NewCallers(PCs{}, "msg").(Callerer).Caller())
+	assert.Equal(t, "errors_location_test.go:34", NewStack(0, 1, "msg").(Callerer).Caller().String())
+	assert.Equal(t, "errors_location_test.go:35", NewStack(0, 1, "msg").(Callerser).Callers().String())
 }
